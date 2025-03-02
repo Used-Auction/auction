7ef51c16be2e8458b3d17b41ba5570f5c5f3a56c
searchcolor <qkrdydwo1022@gmail.com>
2025-03-02 15:55:29 +0900

    create : GlobalExceptionHandler 생성

---
+++ src/main/java/com/example/auction/Global/error/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.example.auction.Global.error.exception;
+
+import com.example.auction.Global.error.errorcode.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
