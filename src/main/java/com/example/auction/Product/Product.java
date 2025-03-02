ae4cfa047870d9d493c62e4f970fffd6ce06d32d
searchcolor <qkrdydwo1022@gmail.com>
2025-03-02 18:05:42 +0900

    feat : 상품 수정 구현

--- src/main/java/com/example/auction/Product/Product.java
+++ src/main/java/com/example/auction/Product/Product.java
@@ -2,7 +2,6 @@ package com.example.auction.Product;
 
 import com.example.auction.Global.BaseEntity;
 import com.example.auction.Product.Dto.ProductRequestDto;
-import com.example.auction.User.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -27,10 +26,20 @@ public class Product extends BaseEntity {
     @Column(columnDefinition = "varchar(320)" )
     private String image;
 
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private ProductStatus productStatus = ProductStatus.ACTIVE;
+
     public Product(){}
 
     public Product(Long userId , ProductRequestDto requestDto){
         this.userId = userId;
+        this.name = requestDto.getName();
+        this.content = requestDto.getContent();
+        this.image = requestDto.getImage();
+    }
+
+    public void updateProduct(ProductRequestDto requestDto){
         this.name = requestDto.getName();
         this.content = requestDto.getContent();
         this.image = requestDto.getImage();
--- src/main/java/com/example/auction/Product/ProductController.java
+++ src/main/java/com/example/auction/Product/ProductController.java
@@ -3,13 +3,9 @@ package com.example.auction.Product;
 import com.example.auction.Global.CommonResponseBody;
 import com.example.auction.Product.Dto.ProductRequestDto;
 import com.example.auction.Product.Dto.ProductResponseDto;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -21,11 +17,11 @@ public class ProductController {
 
     @PostMapping("/add")
     public ResponseEntity<CommonResponseBody<ProductResponseDto>> addProduct(
-            @Valid @RequestBody ProductRequestDto requestDto
+            @RequestBody ProductRequestDto requestDto
             ){
         return ResponseEntity.ok().
-                body(new CommonResponseBody<>("상품 등록"
-                , productService.addProduct(1L,requestDto)));
+                body(new CommonResponseBody<>("상품 등록",
+                        productService.addProduct(1L,requestDto)));
     }
 
     @GetMapping("/{productId}")
@@ -33,6 +29,15 @@ public class ProductController {
         return ResponseEntity.ok().
                 body(new CommonResponseBody<>("상품 조회"
                         , productService.getProduct(productId)));
-        return ResponseEntity.ok().body(new CommonResponseBody<>("제품 등록" , productService.addProduct(1L,requestDto)));
     }
+    
+    @PatchMapping("/{productId}")
+    public ResponseEntity<CommonResponseBody<ProductResponseDto>> updateProduct(
+            @PathVariable Long productId,
+            @RequestBody ProductRequestDto requestDto){
+        return ResponseEntity.ok().
+                body(new CommonResponseBody<>("상품 수정" ,
+                        productService.updateProduct(productId,requestDto)));
+    }
+
 }
