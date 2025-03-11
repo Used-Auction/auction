125d32c95a91f73fa00c8c31ba18aa22658b5e76
searchcolor <qkrdydwo1022@gmail.com>
2025-03-11 17:19:59 +0900

    feat : 포인트 적립 , 입찰 기능 구현

--- src/main/java/com/example/auction/Point/PointController.java
+++ src/main/java/com/example/auction/Point/PointController.java
@@ -1,4 +1,32 @@
 package com.example.auction.Point;
 
+import com.example.auction.Global.CommonResponseBody;
+import com.example.auction.Point.Dto.PointEarnResponseDto;
+import com.example.auction.Point.Dto.PointResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/points")
 public class PointController {
+
+    private final PointService pointService;
+
+    @PostMapping("/earn")
+    public ResponseEntity<CommonResponseBody<PointEarnResponseDto>> earnPoint() {
+
+        return ResponseEntity.ok().body(new CommonResponseBody<>("포인트 적립",
+                pointService.earnPoint(1L,1000)));
+    }
+
+    @PostMapping("/bid")
+    public ResponseEntity<CommonResponseBody<PointResponseDto>> bidPoint(){
+
+        return ResponseEntity.ok().body(new CommonResponseBody<>("경매 입찰 포인트",
+                pointService.bidPoint(1L,1L,2000)));
+    }
 }
--- src/main/java/com/example/auction/Point/PointService.java
+++ src/main/java/com/example/auction/Point/PointService.java
@@ -1,4 +1,55 @@
 package com.example.auction.Point;
 
+import com.example.auction.Auction.Auction;
+import com.example.auction.Auction.AuctionRepository;
+import com.example.auction.Global.error.errorcode.ErrorCode;
+import com.example.auction.Global.error.exception.CustomException;
+import com.example.auction.Point.Dto.PointEarnResponseDto;
+import com.example.auction.Point.Dto.PointResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class PointService {
+
+    private final PointRepository pointRepository;
+    private final AuctionRepository auctionRepository;
+
+    public PointResponseDto bidPoint (Long loginUserId , Long auctionId , int usePoint){
+
+        Auction findAuction = auctionRepository.findByIdOrElseThrow(auctionId);
+        int totalPoint = lastTotalPoint(loginUserId)-usePoint;
+        if (lastTotalPoint(loginUserId) < usePoint){
+            throw new CustomException(ErrorCode.POINT_NOT_ENOUGH);
+        }
+        Point point = new Point(loginUserId
+                ,findAuction
+                ,PointReason.AUCTION_BID
+                ,-usePoint
+                ,totalPoint);
+        pointRepository.save(point);
+
+        return PointResponseDto.toDto(point);
+    }
+
+    public PointEarnResponseDto earnPoint (Long userId , int earnPoint){
+
+        int totalPoint = earnPoint + lastTotalPoint(userId);
+        Point point = new Point(userId,PointReason.EARN,earnPoint,totalPoint);
+        pointRepository.save(point);
+        return PointEarnResponseDto.toDto(point);
+    }
+
+    private int lastTotalPoint(Long userId){
+
+        Optional<Integer> lastTotalPoint = pointRepository.findByLastTotalPoint(userId);
+        if (lastTotalPoint.isPresent()){
+            int p = lastTotalPoint.get();
+            return p;
+        }
+        return 0;
+    }
 }
