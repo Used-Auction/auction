decd9281518d3f4a1360292c17e8d15e9e393915
searchcolor <qkrdydwo1022@gmail.com>
2025-03-11 15:11:38 +0900

    create : PointResponseDto 생성

--- src/main/java/com/example/auction/Point/Dto/PointResponseDto.java
+++ src/main/java/com/example/auction/Point/Dto/PointResponseDto.java
@@ -1,7 +1,29 @@
 package com.example.auction.Point.Dto;
 
+import com.example.auction.Point.PointReason;
 import lombok.Getter;
 
 @Getter
 public class PointResponseDto {
+
+    private final Long id;
+
+    private final Long userId;
+
+    private final Long auctionId;
+
+    private final PointReason reason;
+
+    private final int usePoint;
+
+    private final int totalPoint;
+
+    public PointResponseDto(Long id, Long userId, Long auctionId, PointReason reason, int usePoint, int totalPoint) {
+        this.id = id;
+        this.userId = userId;
+        this.auctionId = auctionId;
+        this.reason = reason;
+        this.usePoint = usePoint;
+        this.totalPoint = totalPoint;
+    }
 }
