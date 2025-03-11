61a558fba5a33a0eb060a32873b30df6edb87ecc
kjs0406 <rnjswlstjr19089@gmail.com>
2025-03-11 15:02:48 +0900

    fix : findByEmail 추가

--- src/main/java/com/example/auction/User/UserRepository.java
+++ src/main/java/com/example/auction/User/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.auction.User;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    default User findUserById(Long id) {
+        return this.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 id의 유저가 존재하지 않습니다."));
+    }
 }
