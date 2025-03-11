e42d1fdb531e8fe0abd4007ccf9447158ac67f3b
SearchColor <152576373+SearchColor@users.noreply.github.com>
2025-03-11 15:07:29 +0900

    fix : PointRequestDto 삭제
    * fix : PointRequestDto 삭제

--- src/main/java/com/example/auction/Point/Dto/PointRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.auction.Point.Dto;
-
-public class PointRequestDto {
-}
--- src/main/java/com/example/auction/Point/Dto/PointResponseDto.java
+++ src/main/java/com/example/auction/Point/Dto/PointResponseDto.java
@@ -1,4 +1,7 @@
 package com.example.auction.Point.Dto;
 
+import lombok.Getter;
+
+@Getter
 public class PointResponseDto {
 }
