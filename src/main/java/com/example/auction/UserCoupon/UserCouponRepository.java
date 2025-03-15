dd110b1d078712cd6c277ab9cb520f302629561d
searchcolor <qkrdydwo1022@gmail.com>
2025-03-15 23:01:46 +0900

    feat : UserCoupon 단건,리스트 조회기능 구현

--- src/main/java/com/example/auction/UserCoupon/UserCouponRepository.java
+++ src/main/java/com/example/auction/UserCoupon/UserCouponRepository.java
@@ -1,16 +1,13 @@
 package com.example.auction.UserCoupon;
 
-
-
 import com.example.auction.Global.error.errorcode.ErrorCode;
 import com.example.auction.Global.error.exception.CustomException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
-
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon,Long> {
 
@@ -18,12 +15,12 @@ public interface UserCouponRepository extends JpaRepository<UserCoupon,Long> {
         return findById(id).orElseThrow(()->new CustomException(ErrorCode.RESOURCES_NOT_FOUND));
     }
 
-    @Query("SELECT cr from UserCoupon cr where cr.userId = :userId and  cr.couponId = :couponId")
-    Optional<UserCoupon> findByUserIdAndCouponId(Long userId, Long couponId);
-
     @Modifying
     @Query(value = "update UserCoupon cr set cr.status = :status where cr.couponId in " +
             "(select c.id from Coupon c where c.expiredAt < now())")
     void expiredCouponRecord(@Param("status") UserCouponStatus status);
 
+    Page<UserCoupon> findByUserId(Long userId, Pageable pageable);
+
+    boolean existsByCouponId(Long couponId);
 }
