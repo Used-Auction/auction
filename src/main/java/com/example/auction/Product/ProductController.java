d25f4a36b848616da0cf864ea4f46f20adb93832
SearchColor <152576373+SearchColor@users.noreply.github.com>
2025-03-26 22:24:26 +0900

    feat : Controller,Service 주석추가

--- src/main/java/com/example/auction/Product/ProductController.java
+++ src/main/java/com/example/auction/Product/ProductController.java
@@ -17,6 +17,12 @@ public class ProductController {
 
     private final ProductService productService;
 
+    /**
+     * <p>상품 등록</p>
+     * @param requestDto 상품 요청 Dto {@link ProductRequestDto}
+     * @param userDetails 로그인유저 Principal 객체
+     * @return ProductResponseDto 상품 응답 Dto {@link ProductResponseDto}
+     */
     @PostMapping("/add")
     public ResponseEntity<CommonResponseBody<ProductResponseDto>> addProduct(
             @RequestBody ProductRequestDto requestDto,
@@ -27,13 +33,24 @@ public class ProductController {
                         productService.addProduct(userDetails.getUser().getId(),requestDto)));
     }
 
+    /**
+     * <p>상품 조회</p>
+     * @param productId 상품 식별자
+     * @return ProductResponseDto 상품 응답 Dto {@link ProductResponseDto}
+     */
     @GetMapping("/{productId}")
     public ResponseEntity<CommonResponseBody<ProductResponseDto>> getProduct(@PathVariable Long productId){
         return ResponseEntity.ok().
                 body(new CommonResponseBody<>("상품 조회"
                         , productService.getProduct(productId)));
     }
-    
+
+    /**
+     * <p>상품 수정</p>
+     * @param productId 상품 식별자
+     * @param requestDto 상품 요청 Dto {@link ProductRequestDto}
+     * @return ProductResponseDto 상품 응답 Dto {@link ProductResponseDto}
+     */
     @PatchMapping("/{productId}")
     public ResponseEntity<CommonResponseBody<ProductResponseDto>> updateProduct(
             @PathVariable Long productId,
@@ -43,6 +60,11 @@ public class ProductController {
                         productService.updateProduct(productId,requestDto)));
     }
 
+    /**
+     * <p>상품 비활성화</p>
+     * @param productId 상품 식별자
+     * @return ProductResponseDto 상품 응답 Dto {@link ProductResponseDto}
+     */
     @DeleteMapping("/{productId}")
     public ResponseEntity<CommonResponseBody<ProductResponseDto>> deleteProduct(
             @PathVariable Long productId){
