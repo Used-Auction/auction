b4285d9a0025b475816357290c28e1f8d3cba11a
searchcolor <qkrdydwo1022@gmail.com>
2025-03-27 03:20:49 +0900

    fix : ResponseDto 수정

--- src/main/java/com/example/auction/UserCoupon/Dto/UserCouponResponseDto.java
+++ src/main/java/com/example/auction/UserCoupon/Dto/UserCouponResponseDto.java
@@ -3,35 +3,23 @@ package com.example.auction.UserCoupon.Dto;
 import com.example.auction.UserCoupon.UserCoupon;
 import com.example.auction.UserCoupon.UserCouponStatus;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class UserCouponResponseDto {
 
     private final Long id;
-
     private final Long userId;
-
     private final Long couponId;
-
     private final UserCouponStatus status;
-
     private final LocalDateTime usedAt;
-
     private final LocalDateTime createdAt;
-
     private final LocalDateTime updatedAt;
-
-    public UserCouponResponseDto(Long id, Long userId, Long couponId, UserCouponStatus status, LocalDateTime usedAt, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.userId = userId;
-        this.couponId = couponId;
-        this.status = status;
-        this.usedAt = usedAt;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 
     public static UserCouponResponseDto toDto(UserCoupon userCoupon){
         return new UserCouponResponseDto(
