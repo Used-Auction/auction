c7dcf61544024590eb03ad0a38f0aa382463a8ba
kjs0406 <127926695+kjs0406@users.noreply.github.com>
2025-03-29 02:06:38 +0900

    fix : kakao login ing.. (#43)
    Co-authored-by: kjs0406 <kjs0406@github.com>

---
+++ src/main/java/com/example/auction/OAuth/Service/OAuthService.java
@@ -0,0 +1,83 @@
+package com.example.auction.OAuth.Service;
+
+import com.example.auction.OAuth.Dto.KakaoTokenDto;
+import com.example.auction.OAuth.Dto.OAuthDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OAuthService {
+
+    private final String grantType = "authorization_code";
+    private final String clientId;
+    private final String redirectUri;
+    private final String clientSecret;
+
+    private static final String KAKAO_TOKEN_URI = "https://kauth.kakao.com/oauth/token";
+
+
+    public OAuthService(
+            @Value("${spring.security.oauth2.client.registration.kakao.client-id}") String clientId,
+            @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")String redirectUri,
+            @Value("${spring.security.oauth2.client.registration.kakao.client-secret}") String clientSecret) {
+        this.clientId = clientId;
+        this.redirectUri = redirectUri;
+        this.clientSecret = clientSecret;
+    }
+
+    public OAuthDto dto (String code) {
+        return new OAuthDto(grantType, clientId, redirectUri, code);
+    }
+
+    @Transactional
+    public KakaoTokenDto getKakaoAccessToken(String code) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // Http Response Body 객체 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code"); //카카오 공식문서 기준 authorization_code 로 고정
+        params.add("client_id", clientId); // 카카오 Dev 앱 REST API 키
+        params.add("redirect_uri", redirectUri); // 카카오 Dev redirect uri
+        params.add("code", code); // 프론트에서 인가 코드 요청시 받은 인가 코드값
+        params.add("client_secret", clientSecret); // 카카오 Dev 카카오 로그인 Client Secret
+
+        // 헤더와 바디 합치기 위해 Http Entity 객체 생성
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // 카카오로부터 Access token 받아오기
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+                KAKAO_TOKEN_URI, // "https://kauth.kakao.com/oauth/token"
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // JSON Parsing (-> KakaoTokenDto)
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        KakaoTokenDto kakaoTokenDto = null;
+        try {
+            kakaoTokenDto = objectMapper.readValue(accessTokenResponse.getBody(), KakaoTokenDto.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return kakaoTokenDto;
+    }
+}
